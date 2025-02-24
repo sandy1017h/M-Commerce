73ae098352cd4727150418226a14af00da94605c
Sidhu1725 <jskusunuri@mitresources.com>
2025-02-24 12:49:45 +0530

    add-product btn in bussdashboard

--- src/app/business-acc-dashboard/business-acc-dashboard-routing.module.ts
+++ src/app/business-acc-dashboard/business-acc-dashboard-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from '../products/add-product/add-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+          path:'addproducts',
+          component:AddProductComponent,
+          pathMatch:'full'
+        }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
